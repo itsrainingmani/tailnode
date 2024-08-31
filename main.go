f9e999e4d8f72a9635c6ba431b76fa80044cf4bb
Manikandan Sundararajan <10191300+itsrainingmani@users.noreply.github.com>
2024-08-31 19:19:00 -0400

    Render the tailscale exit nodes as a table

--- main.go
+++ main.go
@@ -3,154 +3,16 @@ package main
 import (
 	"context"
 	"fmt"
-	"io"
 	"os"
 	"os/exec"
+	"regexp"
 	"runtime"
 	"strings"
-	"time"
 
-	"github.com/charmbracelet/bubbles/list"
+	"github.com/charmbracelet/bubbles/table"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
-
-var (
-	titleStyle        = lipgloss.NewStyle().MarginLeft(2)
-	itemStyle         = lipgloss.NewStyle().PaddingLeft(4)
-	selectedItemStyle = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("170"))
-	paginationStyle   = list.DefaultStyles().PaginationStyle.PaddingLeft(4)
-	helpStyle         = list.DefaultStyles().HelpStyle.PaddingLeft(4).PaddingBottom(1)
-	quitTextStyle     = lipgloss.NewStyle().Margin(1, 0, 2, 4)
-	countries         = []string{
-		"Albania",
-		"Australia",
-		"Austria",
-		"Belgium",
-		"Brazil",
-		"Bulgaria",
-		"Canada",
-		"Colombia",
-		"Croatia",
-		"Czech Republic",
-		"Denmark",
-		"Estonia",
-		"Finland",
-		"France",
-		"Germany",
-		"Greece",
-		"Hong Kong",
-		"Hungary",
-		"Ireland",
-		"Israel",
-		"Italy",
-		"Japan",
-		"Latvia",
-		"Mexico",
-		"Netherlands",
-		"New Zealand",
-		"Norway",
-		"Poland",
-		"Portugal",
-		"Romania",
-		"Serbia",
-		"Singapore",
-		"Slovakia",
-		"South Africa",
-		"Spain",
-		"Sweden",
-		"Switzerland",
-		"UK",
-		"USA",
-		"Ukraine",
-	}
-
-	CountryCityMap = map[string][]string{
-		"Albania":        {"Tirana"},
-		"Australia":      {"Adelaide", "Brisbane", "Melbourne", "Perth", "Sydney"},
-		"Austria":        {"Vienna"},
-		"Belgium":        {"Brussels"},
-		"Brazil":         {"Sao Paulo"},
-		"Bulgaria":       {"Sofia"},
-		"Canada":         {"Calgary", "Montreal", "Toronto", "Vancouver"},
-		"Chile":          {"Santiago"},
-		"Colombia":       {"Bogota"},
-		"Croatia":        {"Zagreb"},
-		"Czech Republic": {"Prague"},
-		"Denmark":        {"Copenhagen"},
-		"Estonia":        {"Tallinn"},
-		"Finland":        {"Helsinki"},
-		"France":         {"Bordeaux", "Marseille", "Paris"},
-		"Germany":        {"Berlin", "Dusseldorf", "Frankfurt"},
-		"Greece":         {"Athens"},
-		"Hong Kong":      {"Hong Kong"},
-		"Hungary":        {"Budapest"},
-		"Indonesia":      {"Jakarta"},
-		"Ireland":        {"Dublin"},
-		"Israel":         {"Tel Aviv"},
-		"Italy":          {"Milan", "Palermo"},
-		"Japan":          {"Osaka", "Tokyo"},
-		"Latvia":         {"Riga"},
-		"Mexico":         {"Queretaro"},
-		"Netherlands":    {"Amsterdam"},
-		"New Zealand":    {"Auckland"},
-		"Norway":         {"Oslo", "Stavanger"},
-		"Peru":           {"Lima"},
-		"Poland":         {"Warsaw"},
-		"Portugal":       {"Lisbon"},
-		"Romania":        {"Bucharest"},
-		"Serbia":         {"Belgrade"},
-		"Singapore":      {"Singapore"},
-		"Slovakia":       {"Bratislava"},
-		"Slovenia":       {"Ljubljana"},
-		"South Africa":   {"Johannesburg"},
-		"Spain":          {"Barcelona", "Madrid", "Valencia"},
-		"Sweden":         {"Gothenburg", "Malmo", "Stockholm"},
-		"Switzerland":    {"Zurich"},
-		"Thailand":       {"Bangkok"},
-		"Turkey":         {"Istanbul"},
-		"UK":             {"Glasgow", "London", "Manchester"},
-		"USA": {
-			"Ashburn", "Atlanta", "Boston", "Chicago", "Dallas", "Denver",
-			"Detroit", "Houston", "LosAngeles", "McAllen", "Miami", "NewYork",
-			"Phoenix", "Raleigh", "SaltLakeCity", "SanJose", "Seattle", "Secaucus",
-		},
-		"Ukraine": {"Kyiv"},
-	}
-)
-
-type item string
-
-type CityInfo struct {
-	City     string
-	Hostname string
-	IP       string
-}
-
-func (i item) FilterValue() string { return "" }
-
-type itemDelegate struct{}
-
-func (d itemDelegate) Height() int                             { return 1 }
-func (d itemDelegate) Spacing() int                            { return 0 }
-func (d itemDelegate) Update(_ tea.Msg, _ *list.Model) tea.Cmd { return nil }
-func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
-	i, ok := listItem.(item)
-	if !ok {
-		return
-	}
-
-	str := fmt.Sprintf("%d. %s", index+1, i)
-
-	fn := itemStyle.Render
-	if index == m.Index() {
-		fn = func(s ...string) string {
-			return selectedItemStyle.Render("> " + strings.Join(s, " "))
-		}
-	}
-
-	fmt.Fprint(w, fn(str))
-}
 
 func changeExitNode(ctx context.Context, exitNode string) error {
 	cmd := exec.CommandContext(ctx, "tailscale", "set", "--exit-node="+exitNode)
@@ -158,68 +20,145 @@ func changeExitNode(ctx context.Context, exitNode string) error {
 	return cmd.Run()
 }
 
-// Define the model which holds the application state
-type model struct {
-	countryList list.Model
-	cityList    list.Model
-	state       string // "country" or "city"
-	choice      string
-	quitting    bool
-}
+func getCurrentExitNode() string {
+	cmd := exec.Command("tailscale", "exit-node", "list")
+	output, err := cmd.Output()
+	if err != nil {
+		return ""
+	}
 
-func (m model) Init() tea.Cmd {
-	return nil
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.WindowSizeMsg:
-		m.countryList.SetWidth(msg.Width)
-		m.cityList.SetWidth(msg.Width)
-		return m, nil
-
-	case tea.KeyMsg:
-		switch keypress := msg.String(); keypress {
-		case "q", "ctrl+c":
-			m.quitting = true
-			time.Sleep(1 * time.Second)
-			return m, tea.Quit
-
-		case "enter":
-			if m.state == "country" {
-				country := string(m.countryList.SelectedItem().(item))
-				m.cityList = list.New(createCityList(country), itemDelegate{}, 20, 40)
-				m.state = "city"
-				return m, nil
-			} else if m.state == "city" {
-				city := string(m.cityList.SelectedItem().(item))
-				m.choice = city
-				time.Sleep(1 * time.Second)
-				return m, tea.Quit
+	lines := strings.Split(string(output), "\n")
+	for _, line := range lines {
+		if strings.Contains(line, "selected") {
+			fields := strings.Fields(line)
+			if len(fields) >= 4 {
+				country := fields[2]
+				city := fields[3]
+				if city == "Any" {
+					continue
+				}
+				return country + ", " + city
 			}
 		}
 	}
 
-	var cmd tea.Cmd
-	if m.state == "country" {
-		m.countryList, cmd = m.countryList.Update(msg)
-	} else {
-		m.cityList, cmd = m.cityList.Update(msg)
+	return ""
+}
+
+func generateMullvadServers() ([]table.Row, error) {
+	cmd := exec.Command("tailscale", "exit-node", "list")
+	output, err := cmd.Output()
+	if err != nil {
+		return nil, fmt.Errorf("failed to execute tailscale command: %v", err)
 	}
+
+	var servers []table.Row
+	lines := strings.Split(string(output), "\n")
+	re := regexp.MustCompile(`\s{2,}`)
+
+	for _, line := range lines[2:] { // Skip the header line
+		if strings.HasPrefix(line, "#") {
+			continue
+		}
+		fields := re.Split(strings.TrimSpace(line), -1)
+		if len(fields) >= 5 {
+			ip := fields[0]
+			hostname := fields[1]
+			country := fields[2]
+			city := fields[3]
+			if city == "Any" {
+				continue
+			}
+			servers = append(servers, table.Row{ip, hostname, country, city})
+		}
+	}
+
+	return servers, nil
+}
+
+// Define the model which holds the application state
+var baseStyle = lipgloss.NewStyle().
+	BorderStyle(lipgloss.NormalBorder()).
+	BorderForeground(lipgloss.Color("240"))
+
+type model struct {
+	table           table.Model
+	execCtx         context.Context
+	currentExitNode string
+	quitting        bool
+	err             error
+}
+
+func initialModel(ctx context.Context, servers []table.Row) model {
+
+	columns := []table.Column{
+		{Title: "IP", Width: 20},
+		{Title: "Hostname", Width: 30},
+		{Title: "Country", Width: 20},
+		{Title: "City", Width: 20},
+	}
+
+	t := table.New(
+		table.WithColumns(columns),
+		table.WithRows(servers),
+		table.WithFocused(true),
+		table.WithHeight(20),
+	)
+
+	s := table.DefaultStyles()
+	s.Header = s.Header.
+		BorderStyle(lipgloss.NormalBorder()).
+		BorderForeground(lipgloss.Color("240")).
+		BorderBottom(true).
+		Bold(false)
+	s.Selected = s.Selected.
+		Foreground(lipgloss.Color("229")).
+		Background(lipgloss.Color("57")).
+		Bold(false)
+	t.SetStyles(s)
+
+	curExitNode := getCurrentExitNode()
+
+	return model{table: t, execCtx: ctx, quitting: false, err: nil, currentExitNode: curExitNode}
+}
+
+func (m model) Init() tea.Cmd { return nil }
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "esc":
+			if m.table.Focused() {
+				m.table.Blur()
+			} else {
+				m.table.Focus()
+			}
+		case "q", "ctrl+c":
+			return m, tea.Quit
+		case "enter":
+			selectedRow := m.table.SelectedRow()
+			exitNodeHostname := selectedRow[1]
+			changeExitNode(m.execCtx, exitNodeHostname)
+			m.currentExitNode = selectedRow[2] + ", " + selectedRow[3]
+			return m, tea.Batch(
+				tea.Printf("Let's go to %s!", m.table.SelectedRow()[1]),
+			)
+		}
+	}
+	m.table, cmd = m.table.Update(msg)
 	return m, cmd
 }
+
 func (m model) View() string {
-	if m.choice != "" {
-		return quitTextStyle.Render(fmt.Sprintf("%s? Sounds good to me.", m.choice))
+	str := baseStyle.Render(m.table.View()) + "\n  " + m.table.HelpView() + "\n"
+	if m.currentExitNode != "" {
+		return str + "\n" + "Current Exit Node is: " + m.currentExitNode
 	}
-	if m.quitting {
-		return quitTextStyle.Render("Not interested? That's cool")
-	}
-	if m.state == "country" {
-		return "\n" + m.countryList.View()
-	}
-	return "\n" + m.cityList.View()
+	return str
 }
+
 func openNewTerminalWithCommand(ctx context.Context) error {
 	var cmd *exec.Cmd
 
@@ -239,34 +178,7 @@ func openNewTerminalWithCommand(ctx context.Context) error {
 	return cmd.Run()
 }
 
-func createCountriesList() []list.Item {
-	var items []list.Item
-	for _, country := range countries {
-		items = append(items, item(country))
-	}
-	return items
-}
-
-func createCityList(country string) []list.Item {
-	var items []list.Item
-	for _, city := range CountryCityMap[country] {
-		items = append(items, item(city))
-	}
-	return items
-}
-
 func main() {
-	countryItems := createCountriesList()
-	m := model{
-		countryList: list.New(countryItems, itemDelegate{}, 20, 40),
-		state:       "country",
-	}
-	m.countryList.Title = "Select a Country"
-	m.countryList.SetShowStatusBar(false)
-	m.countryList.Styles.Title = titleStyle
-	m.countryList.Styles.PaginationStyle = paginationStyle
-	m.countryList.Styles.HelpStyle = helpStyle
-
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	err := openNewTerminalWithCommand(ctx)
@@ -274,6 +186,14 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Failed to open new terminal: %v\n", err)
 		os.Exit(1)
 	}
+
+	servers, err := generateMullvadServers()
+	if err != nil {
+		fmt.Printf("Error getting Tailscale Exit Nodes: %v\n", err)
+		os.Exit(1)
+	}
+
+	m := initialModel(ctx, servers)
 
 	opts := []tea.ProgramOption{
 		tea.WithContext(ctx), // Pass the context to the program
